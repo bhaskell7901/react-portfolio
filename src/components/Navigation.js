c2cbf1ba242e70cdd5475536a422491df9606fb5
Brandon Haskell <bdh747flyna1@hotmail.com>
2023-03-05 14:24:46 -0800

    add Header and Navigation files

---
+++ src/components/Navigation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Navigation(props) {
+    return (
+        <nav>
+            <ul>
+                <li><a href="#bio">Bio</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#email">Contact</a></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navigation;
