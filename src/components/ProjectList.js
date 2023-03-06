4dbbb90c53ab20dae878710e260cac438623185a
Brandon Haskell <bdh747flyna1@hotmail.com>
2023-03-05 16:33:57 -0800

    add map function to ProjectList file

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,6 +2,20 @@ import React from 'react';
 import Project from '../components/Project'
 
 function ProjectList(props) {
+    const projectData = [
+        {
+            id: 1,
+            projectUrl: 'https://bhaskell7901.github.io/know-your-government/index.html',
+            projectTitle: 'Know Your Government',
+            img: {
+                src: "filename",
+                altTxt: "close up of laptop being used"
+            }
+        }
+    ];
+
+    const projectList = projectData.map(item => <Project key={item.id} name={item.name} />);
+
     return (
         <section id="projects">
             <h2>Projects</h2>
